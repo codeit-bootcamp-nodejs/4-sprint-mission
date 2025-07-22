64713a489d979a2fbedec333c70f0c904bc46680
ehdehd <ehdehd0175@gmail.com>
2025-07-22 14:30:45 +0900

    mission-2

---
+++ mission-2/Product.js
@@ -0,0 +1,33 @@
+export default class Product {
+    #name;
+    #price;
+    #tags;
+    #images;
+    #favoriteCount;
+    #description;
+    constructor({ name = 'default', description = 'default', price = 'default', tags = 'default', images = 'default', favoriteCount = 'default' }) {
+        this.#name = name;
+        this.#description = description;
+        this.#price = price;
+        this.#tags = tags;
+        this.#images = images;
+        this.#favoriteCount = favoriteCount;
+    }
+    // getter, setter
+    getName() { return this.#name; }
+    getPrice() { return this.#price; }
+    getTags() { return this.#tags; }
+    getImages() { return this.#images; }
+    getFavoriteCount() { return this.#favoriteCount; }
+    getDescription() { return this.#description; }
+    setName(name) { this.#name = name; }
+    setPrice(price) { this.#price = price; }
+    setTags(tags) { this.#tags = tags; }
+    setImages(images) { this.#images = images; }
+    setFavoriteCount(favoriteCount) { this.#favoriteCount = favoriteCount; }
+    setDescription(description) { this.#description = description; }
+
+    favorite() {
+        this.#favoriteCount++;
+    }
+}
