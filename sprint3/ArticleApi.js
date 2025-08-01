50de3fe6af2a6c62afb63e01af7b027ecd35c37e
Son JuneYeong <sonj0407@naver.com>
2025-08-01 13:47:52 +0900

    feat: made Article API

---
+++ sprint3/ArticleApi.js
@@ -0,0 +1,54 @@
+import prisma from 'prisma'
+import express from 'express'
+
+const ArticleRouter = express.Router();
+
+ArticleRouter.get('/', (req,res) =>{
+    const {sort, offset, title, content} = req.body;
+    const Articles = prisma.Article.findMany({
+
+    })
+});
+
+
+ArticleRouter.get('/:id', (req,res) =>{
+    const id = req.params.id;
+    const Article = prisma.Article.findUnique({
+        data: {id};
+    });
+    res.send(Article);
+});
+
+
+
+ArticleRouter.post('/', (req,res) =>{
+    const {title, content} = req.body;
+    const Article =  prisma.Article.create({
+        data: {
+            title,
+            content
+        }
+    });
+    res.send(Article);
+});
+
+
+ArticleRouter.patch('/:id', (req,res) =>{
+    const {title,content} = req.body; 
+    const id = req.params.id;
+    const Article = prisma.Article.update({
+        data: {
+            title,
+            content
+        }
+    })
+} )
+
+
+ArticleRouter.delete('/:id', (req,res) =>{
+    const id = req.params.id;
+    prisma.Article.delete({
+        where:{id}
+    });
+    console.log("data deleting success")
+} )
