97be70fb323fc31e5cb201041693a28bae1f1c7f
오연진 <yonjin.oh@gmail.com>
2025-08-04 17:17:16 +0900

    fix: 폴더 구조 변경

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { testAllArticleService } from "./external/tests/testArticleService.js";
+import { testAllProductService } from "./external/tests/testProductService.js";
+
+//testAllArticleService();
+testAllProductService();
