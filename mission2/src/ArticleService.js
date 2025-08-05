98ed02347887f835d75ce0b7c854924aeedff03d
baturs <bill0302@naver.com>
2025-08-05 17:09:47 +0900

    modified files

--- mission2/src/ArticleService.js
+++ mission2/src/ArticleService.js
@@ -1,3 +1,4 @@
+//getArticleList
 export async function getArticleList(params) {
     const url = new URL(`https://panda-market-api-crud.vercel.app/articles`);
     Object.keys(params).forEach((key) =>
@@ -9,12 +10,14 @@ export async function getArticleList(params) {
     return data;
 }
 
+//getArticle
 export async function getArticle() {
     const res = await fetch (`https://panda-market-api-crud.vercel.app/articles`);
     const data = await res.json();
     return data;
 }
 
+//createArticle
 export async function createArticle(articleData) {
   const res = await fetch(`https://panda-market-api-crud.vercel.app/articles`, {
     method: 'POST',
@@ -28,6 +31,7 @@ export async function createArticle(articleData) {
   return data;
 }
 
+//patchArticle
 export async function patchArticle(id, articleData) {
     const res = await fetch(`https://panda-market-api-crud.vercel.app/articles/${id}`, {
         method: 'PATCH',
@@ -40,6 +44,7 @@ export async function patchArticle(id, articleData) {
     return data;
 }
 
+//deleteArticle
 export async function deleteArticle(id) {
     const res = await fetch(`https://panda-market-api-crud.vercel.app/articles/${id}`, {
         method: 'DELETE'
@@ -47,17 +52,3 @@ export async function deleteArticle(id) {
     const data = await res.json();
     return data;
 }
-
- import axios from 'axios';
-
- axios.post(`https://panda-market-api-crud.vercel.app/articles`, {
-    title:'게시글 작성 테스트',
-    content: '게시글 작성 테스트',
-    image: 'https://example.com/...'
- })
- .then(response => {
-    console.log('응답 데이터:', response.data);
- })
- .catch(error => {
-    console.error('에러 발생:', error);
- });
--- mission2/src/main.js
+++ mission2/src/main.js
@@ -1,13 +1,16 @@
 //Article ---
 
- import { getArticleList, getArticle, createArticle, patchArticle,  deleteArticle } from "./ArticleService.js";
+ import { getArticleList, getArticle, createArticle, patchArticle,  deleteArticle } from './ArticleService.js';
 
+ //getArticleList
  const data1 = await getArticleList('page', 'pageSize', 'keyword');
  console.log(data1);
 
+ //getArticle
  const data2 = await getArticle();
  console.log(data2);
 
+ //createArticle
  const articleData = {
      title: '게시글 작성 테스트12345',
      content: '게시글 작성 테스트입니다.',
@@ -17,7 +20,7 @@
  const data3 = await createArticle(articleData);
  console.log(data3);
 
-
+//patchArticle
 let article = await createArticle({
     title: '게시글 작성 테스트3',
     content: '게시글 작성 테스트입니다.',
@@ -33,19 +36,22 @@ article = await patchArticle(article.id, {
 });
 
 console.log(article);
+
+//deleteArticle
 await deleteArticle(article.id);
 
+// 비동기 처리, 오류 처리
 fetch(`https://panda-market-api-crud.vercel.app/docs/articles`)
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((error) => console.log('Error!'))
   .finally(() => console.log('Finished'));
 
-async function getArticle() {
-    const response = await fetch(`https://panda-market-api-crud.vercel.app/articles`);
-    const data = await response.json();
-    console.log(data);
-};
+// async function getArticle() {
+//     const response = await fetch(`https://panda-market-api-crud.vercel.app/articles`);
+//     const data = await response.json();
+//     console.log(nodpe data);
+// };
 
 fetch(`https://panda-market-api-crud.vercel.app/articles`)
   .then((response) => response.json())
@@ -53,36 +59,41 @@ fetch(`https://panda-market-api-crud.vercel.app/articles`)
 
 console.log('게시물을 불러내고 있습니다...');
 
-//Product---
 
-import { getProductList, getProduct, createProduct, patchProduct, deleteProduct, checkProduct } from "./ProductServicejs";
+// //Product---
 
-const data4 = await getProductList('page', 'pageSize', 'keyword');
-console.log(data4);
+// import { getProductList, getProduct, createProduct, patchProduct, deleteProduct, checkProduct } from "./ProductServicejs";
 
-const data5 = await getProduct();
-console.log(data5);
+// //getProductList
+// const data4 = await getProductList('page', 'pageSize', 'keyword');
+// console.log(data4);
 
-const productData = {
-    name: '디지몬 피규어',
-    description: '가슴이 웅장해지는 오메가몬',
-    price: 80000,
-    tags: '장식',
-    images: "https://example.com/...",
-};
+// //getProduct
+// const data5 = await getProduct();
+// console.log(data5);
 
-const data6 = await createProduct(productData);
-console.log(data6);
+// //createdProduct
+// const productData = {
+//     name: '디지몬 피규어',
+//     description: '가슴이 웅장해지는 오메가몬',
+//     price: 80000,
+//     tags: '장식',
+//     images: "https://example.com/...",
+// };
 
-let products = await createProduct({
-    name: '디지몬 피규어',
-    description: '가슴이 웅장해지는 오메가몬',
-    price: 80000,
-    tags: '장식',
-    images: "https://example.com/...",
-});
+// const data6 = await createProduct(productData);
+// console.log(data6);
 
-console.log(products);
+// //patchProduct
+// let products = await createProduct({
+//     name: '디지몬 피규어',
+//     description: '가슴이 웅장해지는 오메가몬',
+//     price: 80000,
+//     tags: '장식',
+//     images: "https://example.com/...",
+// });
+
+// console.log(products);
 
 // const product = await patchProduct(id, {
 //     name: '디지털 몬스터',
@@ -94,14 +105,57 @@ console.log(products);
 
 // console.log(product);
 
+// //deleteProduct
 // await deleteProduct(1258);
 
-import { checkProduct } from "./ProductService.js";
+// //비동기 처리, 오류 처리
+// import { checkProduct } from "./ProductService.js";
 
-try {
-    const data = await checkProduct(id);
-    console.log(data);
-} catch (e) {
-    console.log(`오류가 발생했습니다.`)
-    console.log(e.message);
-}
+// try {
+//     const data = await checkProduct(id);
+//     console.log(data);
+// } catch (e) {
+//     console.log(`오류가 발생했습니다.`)
+//     console.log(e.message);
+// }
+
+// //해시태그 '전자제품' 분리
+// function addNewProduct(products) {
+//   const { name, description, price, tags, images, favoriteCount } = product;
+//   const newProduct = {
+//     id: products.length + 1,
+//     name,
+//     description,
+//     price,
+//     tags,
+//     images,
+//     favoriteCount,
+//   };
+//   productsInput.push(newProduct);
+// }
+
+// const productsInput = await getProduct();
+
+// console.log(productsInput);
+
+// const products = productsInput;
+
+// console.log(products);
+
+// const electronicProduct = [];
+
+// // productsInput.list.forEach((product) => {
+// //   if(product.tags.find((element) => element === '전자제품')) {
+// //     electronicProduct.push(product);
+// //   }
+// // });
+
+// // console.log(electronicProduct);
+
+// productsInput.list.forEach((product) => {
+//   if(product.tags.includes('전자제품')) {
+//     electronicProduct.push(product);
+//   }
+// });
+
+// console.log(electronicProduct);
