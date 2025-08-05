6ecafc98fa882c61c826df91aa2f3261d00e34d5
오연진 <yonjin.oh@gmail.com>
2025-08-05 17:05:38 +0900

    feat: multer 이미지 업로드 API 구현

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ProductRouter from "./api/routes/ProductRouter.js";
 import ArticleRouter from "./api/routes/ArticleRouter.js";
 import CommentRouter from "./api/routes/CommentRouter.js";
 import errorHandler from "./api/middlewares/errorHandler.js";
-
+import imageRouter from "./api/routes/ImageRoute.js";
 // import { testAllArticleService } from "./external/tests/testArticleService.js";
 // import { testAllProductService } from "./external/tests/testProductService.js";
 
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use("/products", ProductRouter);
 app.use("/articles", ArticleRouter);
 app.use("/comments", CommentRouter);
+
+app.use("/uploads", express.static("uploads"));
+app.use("/images", imageRouter);
 
 app.use(errorHandler);
 
