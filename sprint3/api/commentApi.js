b74468c8f465cc9bddc48d4b38f82d2486a5b4fe
Hoon <dlgnswp21@gmail.com>
2025-08-05 11:03:51 +0900

    feat: add create product comment API

---
+++ sprint3/api/commentApi.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.post("/products/:produtId/comments", async (req, res) => {
+  const { productId } = req.params;
+  const { content } = req.body;
+
+  if (!productId || !content) {
+    return res.status(400).json({ error: "productId와 content는 필수입니다." });
+  }
+
+  try {
+    const comment = await prisma.comment.create({
+      data: {
+        content,
+        product: { connect: { id: parseInt(productId) } },
+      },
+    });
+
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(500).json({ error: "댓글 등록 실패", code: err.code });
+  }
+});
