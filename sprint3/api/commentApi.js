772fa70bde0d08468f9e746202f43deb6f63ecd4
Hoon <dlgnswp21@gmail.com>
2025-08-06 14:46:44 +0900

    fix: remove POST routes for articles

--- sprint3/api/commentApi.js
+++ sprint3/api/commentApi.js
@@ -29,30 +29,7 @@ router.post("/:productId/comments", async (req, res) => {
   }
 });
 
-router.post("/articles/:articleId/comments", async (req, res) => {
-  const articleId = Number(req.params.articleId);
-  const { content } = req.body;
-
-  if (isNaN(articleId) || !content) {
-    return res
-      .status(400)
-      .json({ error: "articleId와 content는 필수입니다. " });
-  }
-
-  try {
-    const comment = await prisma.comment.create({
-      data: {
-        content,
-        article: { connect: { id: articleId } },
-      },
-    });
-    res.status(201).json(comment);
-  } catch (err) {
-    res.status(500).json({ error: "댓글 등록 실패", code: err.code });
-  }
-});
-
-router.patch("/products/:productId/comments/:commentId", async (req, res) => {
+router.patch("/:productId/comments/:commentId", async (req, res) => {
   const commentId = Number(req.params.commentId);
   const productId = Number(req.params.productId);
   const { content } = req.body;
