eee338f75cca1f82e5d0c9d8a2c2f645abed1825
ehdehd <ehdehd0175@gmail.com>
2025-08-07 13:20:18 +0900

    feat: add test.http and fileRouter

--- mission-3/app.js
+++ mission-3/app.js
@@ -1,14 +1,22 @@
 import express from 'express';
-import productRouter from './router/product.js';
-import articleRouter from './router/article.js';
+import productRouter from './routers/product.js';
+import articleRouter from './routers/article.js';
+import jsonErrorHandler from './middlewares/jsonErrorHandler.js';
+import fileRouter from './routers/file.js';
 
 const app = express();
 const PORT = 3000;
 
-app.use('/product', productRouter);
+app.use(express.json());
+app.use(express.static('uploads'));
+app.use(express.urlencoded({ extended: true }))
 
+app.use('/product', productRouter);
 app.use('/article', articleRouter);
+app.use('/uploads', fileRouter);
+
+app.use(jsonErrorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`서버가 ${PORT}에서 실행중입니다.`)
-})
+});
