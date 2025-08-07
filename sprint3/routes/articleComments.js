6c6e03004845d392c83cd625c2d359adad7ebe87
Hoon <dlgnswp21@gmail.com>
2025-08-07 11:26:58 +0900

    feat: add update article comment API

--- sprint3/routes/articleComments.js
+++ sprint3/routes/articleComments.js
@@ -28,3 +28,35 @@ router.post("/:articleId/comments", async (req, res) => {
     res.status(500).json({ error: "댓글 등록 실패", code: err.code });
   }
 });
+
+router.patch("/:articleId/comments/:commentId", async (req, res) => {
+  const commentId = Number(req.params.commentId);
+  const articleId = Number(req.params.articleId);
+  const { content } = req.body;
+
+  if (isNaN(commentId) || isNaN(articleId) || !content) {
+    return res.status(400).json({ error: "필수 항목 누락" });
+  }
+
+  try {
+    const comment = await prisma.comment.findFirst({
+      where: {
+        id: commentId,
+        articleId: articleId,
+      },
+    });
+
+    if (!comment) {
+      return res.status(404).json({ error: "댓글을 찾을 수 없습니다." });
+    }
+
+    const updated = await prisma.comment.update({
+      where: { id: commentId },
+      data: { content },
+    });
+
+    res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ error: "댓글 업데이트 실패", code: err.code });
+  }
+});
