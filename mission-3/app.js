cdad22717d4321d42b2e6c681a136e688a7c8a22
ehdehd <ehdehd0175@gmail.com>
2025-08-07 16:26:18 +0900

    feat: add cors and deploy setting

--- mission-3/app.js
+++ mission-3/app.js
@@ -3,10 +3,12 @@ import productRouter from './routers/product.js';
 import articleRouter from './routers/article.js';
 import jsonErrorHandler from './middlewares/jsonErrorHandler.js';
 import fileRouter from './routers/file.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.static('uploads'));
 app.use(express.urlencoded({ extended: true }))
