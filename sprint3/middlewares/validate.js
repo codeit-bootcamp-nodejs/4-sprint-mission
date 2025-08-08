46cd15b978ed59dd062ff86b84bc2674dc9c7592
Hoon <dlgnswp21@gmail.com>
2025-08-08 11:28:56 +0900

    feat: add validation for query parameters and ID

--- sprint3/middlewares/validate.js
+++ sprint3/middlewares/validate.js
@@ -1,7 +1,7 @@
 export function validateProdCreate(req, res, next) {
-  const { name, price, tags } = req.body;
+  const { title, price, tags } = req.body;
 
-  if (!name || !price) {
+  if (!title || !price) {
     return res.status(400).json({ error: "필수 항목 누락" });
   }
   if (tags !== undefined) {
@@ -27,6 +27,37 @@ export function validateProdUpdate(req, res, next) {
   next();
 }
 
+export function validateProdQuery(req, res, next) {
+  const allowedKeys = ["offset", "limit", "name", "description"];
+
+  const queryKeys = Object.keys(req.query);
+
+  for (const key of queryKeys) {
+    if (!allowedKeys.includes(key)) {
+      return res.status(400).json({ error: `잘못된 파라미터: ${key}` });
+    }
+  }
+
+  const { name, description } = req.query;
+
+  if (name !== undefined && (typeof name !== "string" || name.trim() === "")) {
+    return res
+      .status(400)
+      .json({ error: "name은은 공백 없는 문자열이여야합니다." });
+  }
+
+  if (
+    description !== undefined &&
+    (typeof description !== "string" || description.trim() === "")
+  ) {
+    return res
+      .status(400)
+      .json({ error: "description는 공백 없는 문자열이여야합니다." });
+  }
+
+  next();
+}
+
 export function validateArtCreate(req, res, next) {
   const { title, content } = req.body;
 
@@ -37,11 +68,57 @@ export function validateArtCreate(req, res, next) {
   next();
 }
 
+export function validateArtQuery(req, res, next) {
+  const allowedKeys = ["offset", "limit", "title", "content"];
+
+  const queryKeys = Object.keys(req.query);
+
+  for (const key of queryKeys) {
+    if (!allowedKeys.includes(key)) {
+      return res.status(400).json({ error: `잘못된 파라미터: ${key}` });
+    }
+  }
+
+  const { title, content } = req.query;
+
+  if (
+    title !== undefined &&
+    (typeof title !== "string" || title.trim() === "")
+  ) {
+    return res
+      .status(400)
+      .json({ error: "title은 공백 없는 문자열이여야합니다." });
+  }
+
+  if (
+    content !== undefined &&
+    (typeof content !== "string" || content.trim() === "")
+  ) {
+    return res
+      .status(400)
+      .json({ error: "content는 공백 없는 문자열이여야합니다." });
+  }
+
+  next();
+}
+
 export function validateNewComment(req, res, next) {
   const { content } = req.body;
 
   if (!content) {
     return res.status(400).json({ error: "content는 필수입니다." });
+  }
+
+  next();
+}
+
+export function validateId(req, res, next) {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res
+      .status(400)
+      .json({ error: "잘못된 ID 입니다. 숫자여야 합니다." });
   }
 
   next();
