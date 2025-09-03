afdff7b2264199bc576931bf63a64b0a2d73d8a3
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-09-03 14:47:41 +0900

    refactor: 메인 파일 수정 및 기존 게시글 코드 제거

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import express from 'express';
 import cors from 'cors';
 
 import container from './src/container.js';
-import productsRouter from './routes/ProductRoute.js';
-import articlesRouter from './routes/ArticleRoute.js';
-import uploadsRouter from './routes/ImageRoute.js';
-import commentsRouter from './routes/CommentRoute.js';
+import productRouter from './src/route/product-router.js';
+import articleRouter from './src/route/article-router.js';
+import uploadRouter from './src/route/upload-router.js';
+import commentRouter from './src/route/comment-router.js';
 import { errorHandler } from './middleware/ErrorHandlerMiddleware.js';
 
 dotenv.config();
@@ -18,12 +18,12 @@ app.use(cors());
 
 app.use(express.json());
 
-const { productController } = container;
+const { productController, articleController } = container;
 
-app.use('/products', productsRouter(productController));
-app.use('/articles', articlesRouter);
-app.use('/comments', commentsRouter);
-app.use('/uploads', uploadsRouter);
+app.use('/products', productRouter(productController)); // 미들웨어는 추후 적용 예정
+app.use('/articles', articleRouter(articleController)); // 미들웨어는 추후 적용 예정
+app.use('/comments', commentRouter);
+app.use('/uploads', uploadRouter);
 
 app.use(errorHandler);
 
