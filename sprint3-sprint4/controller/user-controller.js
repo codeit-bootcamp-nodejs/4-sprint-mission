8bb102f1168e45fef2fe41485e7456baff6eb441
Son JuneYeong <sonj0407@naver.com>
2025-09-03 17:49:19 +0900

    feat: add userController

--- sprint3-sprint4/controller/user-controller.js
+++ sprint3-sprint4/controller/user-controller.js
@@ -10,7 +10,7 @@ class UserController{
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(plainPassword,hash);
 
-        const newUser =  prisma.user.create({
+        const newUser = prisma.user.create({
             data:{
                 password: hashedPassword,
                 email,
@@ -19,9 +19,56 @@ class UserController{
         })
     }
 
-    login = (req,res,next) => {
+    login = async(req,res,next) => {
+        const {email, password} = req.body;
+        const user = prisma.user.findUnique({}) 
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch){
 
+        }
+        return 
     }
 
+    getUser = async(req,res,next) => {
+        const userId = req.params.userId;
+        const user = prisma.user.findUnique({
+            where:{id:userId}
+        })
+
+        if (!user){
+
+        }
+
+        return user
+    }
+
+    patchUser = async(req,res,next) => {
+        const {nickname, image} = req.body;
+        const patchUser = prisma.user.update({
+            data:{nickname,image}
+        })
+        return patchUser
+    }
+
+    patchPassword = async(req,res,next) => {
+        const {password} = req.body;
+        const patchUser = prisma.user.update({
+            data:{password}
+        })
+        return patchUser
+    }
+
+    getUserProduct = async(req,res, next) => {
+        const userId = req.params.userId;
+        const user = prisma.user.findUnique({
+            where:{id: userId},
+            include:{product:true}
+        })
+        const userProduct = user.product
+
+        return userProduct
+    }
 
 }
+
+export default new UserController();
