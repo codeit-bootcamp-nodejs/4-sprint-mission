cb923a94e426c8e51917da97efdd84883656bd80
Son JuneYeong <sonj0407@naver.com>
2025-09-03 17:49:47 +0900

    feat: connect url with controller

--- sprint3-sprint4/router/user-router.js
+++ sprint3-sprint4/router/user-router.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import userController from '../controller/user-controller';
+import passport from 'passport';
 
 userRouter = express.router();
 
-userRouter.get('/:userId')
-userRouter.get(':userID/products')
-userRouter.patch('/:userId')
+
+userRouter.post('/login', userController.login)
+userRouter.post('/register', userController.register)
+
+userRouter.get('/:userId', userController.getUser)
+userRouter.patch('/:userId', userController.patchUser)
+userRouter.patch('/:userId', userController.patchPassword)
+
+userRouter.get(':userID/products', userController.getUserProduct)
+
+
+// passport.authenticate('local'),
+// passport.authenticate('jwt')
 
 
 export default userRouter;
