564bcaff193b3896309ee18fe1d2ee65b94e482a
Son JuneYeong <sonj0407@naver.com>
2025-09-05 16:15:04 +0900

    chore: change format

--- sprint3-sprint4/router/user-router.js
+++ sprint3-sprint4/router/user-router.js
@@ -6,23 +6,30 @@ import checkAuthenticated from '../middleware/auth-middleware.js';
 userRouter = express.router();
 
 // register, login, get login user information
-userRouter.post('/login', userController.login)
-userRouter.post('/register', userController.register)
-userRouter.get('/:id', passport.authenticate('AccessToken', {session:false}) ,
+userRouter.post('/login', 
+    userController.login)
+userRouter.post('/register', 
+    userController.register)
+userRouter.get('/:id', 
+    passport.authenticate('AccessToken', {session:false}) ,
     userController.getUser)
 
 //modify user information
-userRouter.patch('/:id', passport.authenticate('AccessToken', {session:false}) ,
+userRouter.patch('/:id', 
+    passport.authenticate('AccessToken', {session:false}) ,
     userController.patchUser)
-userRouter.patch('/:id', passport.authenticate('AccessToken', {session:false}) ,
+userRouter.patch('/:id',
+     passport.authenticate('AccessToken', {session:false}) ,
      userController.patchPassword)
 
 //get products of login user
-userRouter.get(':id/products', passport.authenticate('AccessToken', {session:false}) ,
+userRouter.get(':id/products',
+     passport.authenticate('AccessToken', {session:false}) ,
      userController.getUserProduct)
 
 //get liked products of login user
-userRouter.get(':id/like', passport.authenticate('AccessToken', {session:false}) ,
+userRouter.get(':id/like', 
+    passport.authenticate('AccessToken', {session:false}) ,
 
 )
 
