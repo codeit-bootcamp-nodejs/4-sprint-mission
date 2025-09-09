d8e55f5a3f63ff915b769362b9ec00b68039d74c
hj92oo <scfe97@naver.com>
2025-09-09 13:36:10 +0900

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { errorHandler } from './sprint3/error.js';
 import articlesRoute from './sprint3/route/articles-route.js';
 import productsRoute from './sprint3/route/products-route.js';
@@ -7,6 +8,13 @@ import productsRoute from './sprint3/route/products-route.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
+
+app.use(cors({
+    origin : [
+      'http://localhost:3000'
+    ],
+    credentials: true
+}));
 
 app.use('/', articlesRoute);
 app.use('/', productsRoute);
