295f482c9b257d514af46bc100b5fe6fb4f50cd6
오연진 <yonjin.oh@gmail.com>
2025-09-10 20:34:14 +0900

    refactor: routes 및 main.ts ts로 변경 & @types/cookie-parser install

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import express from "express";
-// import ProductRouter from "./api/routes/ProductRouter.js";
-// import ArticleRouter from "./api/routes/ArticleRouter.js";
-// import CommentRouter from "./api/routes/CommentRouter.js";
-// import errorHandler from "./api/middlewares/errorHandler.js";
-// import imageRouter from "./api/routes/ImageRouter.js";
-// import AuthRouter from "./api/routes/AuthRouter.js";
-// import MypageRouter from "./api/routes/MypageRouter.js";
-// import LikeRouter from "./api/routes/LikeRouter.js";
-// import cookieParser from "cookie-parser";
+import ProductRouter from "./api/routes/ProductRouter.js";
+import ArticleRouter from "./api/routes/ArticleRouter.js";
+import CommentRouter from "./api/routes/CommentRouter.js";
+import errorHandler from "./api/middlewares/errorHandler.js";
+import imageRouter from "./api/routes/ImageRouter.js";
+import AuthRouter from "./api/routes/AuthRouter.js";
+import MypageRouter from "./api/routes/MypageRouter.js";
+import LikeRouter from "./api/routes/LikeRouter.js";
+import cookieParser from "cookie-parser";
 
 // import { testAllArticleService } from "./external/tests/testArticleService.js";
 // import { testAllProductService } from "./external/tests/testProductService.js";
@@ -19,18 +19,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// app.use("/products", ProductRouter);
-// app.use("/articles", ArticleRouter);
-// app.use("/comments", CommentRouter);
-// app.use("/mypage", MypageRouter);
-// app.use("/likes", LikeRouter);
+app.use("/products", ProductRouter);
+app.use("/articles", ArticleRouter);
+app.use("/comments", CommentRouter);
+app.use("/mypage", MypageRouter);
+app.use("/likes", LikeRouter);
 
-// app.use("/uploads", express.static("uploads"));
-// app.use("/images", imageRouter);
-// app.use("/auth", AuthRouter);
-// app.use(errorHandler);
+app.use("/uploads", express.static("uploads"));
+app.use("/images", imageRouter);
+app.use("/auth", AuthRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
