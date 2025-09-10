d74a78683eea82a003d230d57a99b4ba92b417dd
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-09-10 18:03:04 +0900

    feat: 라우터 코드에 관련 라우터 적용

--- src/route/user-router.js
+++ src/route/user-router.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { authMiddleware } from '../middleware/auth-middleware.js';
 
 const userRouter = (userController) => {
   const router = express.Router();
 
   router.post('/signup', userController.signUp);
   router.post('/signin', userController.signIn);
+
+  // '내 정보' 관련 라우트 추가
+  router
+    .route('/me')
+    .get(authMiddleware, userController.getMyInfo) // 내 정보 조회
+    .patch(authMiddleware, userController.updateMyInfo); // 내 정보 수정
 
   return router;
 };
