01de8e11ac79c72746fa170d0b037f8dc11a7154
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-09-10 16:49:02 +0900

    feat: id로 사용자 찾기, 사용자 정보 수정을 위한 컨트롤러 코드 작성

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -27,4 +27,33 @@ export class UserController {
       next(error);
     }
   };
+
+  // 내 정보 조회
+  getMyInfo = async (req, res, next) => {
+    try {
+      const { id: userId } = req.user;
+      const userInfo = await this.userService.getUserInfo(userId);
+      res.status(200).json({ data: userInfo });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // 내 정보 수정
+  updateMyInfo = async (req, res, next) => {
+    try {
+      const { id: userId } = req.user;
+      const userInfoData = req.body;
+      const updatedUser = await this.userService.updateUserInfo(
+        userId,
+        userInfoData,
+      );
+      res.status(200).json({
+        message: '사용자 정보가 성공적으로 수정되었습니다.',
+        data: updatedUser,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
