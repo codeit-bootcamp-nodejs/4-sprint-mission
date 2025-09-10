dd43f5101026dc04e778418dbd22d42bad07c0fa
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-09-10 11:12:25 +0900

    refactor: main.js 파일 수정

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ import express from 'express';
 import cors from 'cors';
 
 import container from './src/container.js';
-import productRouter from './src/route/product-router.js';
-import articleRouter from './src/route/article-router.js';
-import uploadRouter from './src/route/upload-router.js';
-import commentRouter from './src/route/comment-router.js';
-import { errorHandler } from './middleware/ErrorHandlerMiddleware.js';
+import productRouter from './src/routes/product.route.js';
+import articleRouter from './src/routes/article.route.js';
+import imageRouter from './src/routes/image.route.js';
+import { errorHandler } from './src/middleware/error-handler.middleware.js';
 
 dotenv.config();
 
@@ -15,15 +14,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
-const { productController, articleController, commentController } = container;
+const {
+  productController,
+  articleController,
+  commentController,
+  imageController,
+  validationMiddleware,
+  imageMiddleware,
+} = container;
 
-app.use('/products', productRouter(productController)); // 미들웨어는 추후 적용 예정
-app.use('/articles', articleRouter(articleController)); // 미들웨어는 추후 적용 예정
-app.use('/comments', commentRouter(commentController));
-app.use('/uploads', uploadRouter);
+app.use(
+  '/products',
+  productRouter(productController, commentController, validationMiddleware),
+);
+app.use(
+  '/articles',
+  articleRouter(articleController, commentController, validationMiddleware),
+);
+app.use('/uploads', imageRouter(imageController, imageMiddleware));
 
 app.use(errorHandler);
 
