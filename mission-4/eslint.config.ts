654e6f55de110055abb14a2bcfdef91a67aa711a
aprkal12 <ehdehd0175@gmail.com>
2025-09-11 09:36:25 +0900

    feat: 타입스크립트 변환을 위한 패키지, 설정 파일 추가
    변동 사항
    eslint, prettier, tsconfig 연동을 위한 세팅
    
    타입스크립트 추가 패키지 설치
    
    데이터 스키마 수정

---
+++ mission-4/eslint.config.ts
@@ -0,0 +1,41 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  // 기본 및 TypeScript 규칙 적용
+  // 기존 extends의 "eslint:recommended", "plugin:@typescript-eslint/recommended"에 해당합니다.
+  ...tseslint.configs.recommended,
+
+  // 프로젝트 전역 설정
+  {
+    // 기존 ignorePatterns에 해당합니다.
+    ignores: ['dist/', 'node_modules/'],
+    // 언어 및 환경 설정
+    languageOptions: {
+      // 파서 및 파서 옵션 설정
+      // 기존 parser, parserOptions에 해당합니다.
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+      },
+
+      // 실행 환경 설정
+      // 기존 env에 해당합니다.
+      globals: {
+        ...globals.node,
+        ...globals.es2024,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+  },
+  // Prettier와 충돌되는 규칙 비활성화
+  // 기존 extends의 "plugin:prettier/recommended", "prettier/@typescript-eslint"에 해당합니다.
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+]);
