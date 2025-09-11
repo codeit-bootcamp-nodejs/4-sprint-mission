f7cb9e39e18f0215de38d1b006ed13a6ffe30150
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-09-11 10:42:10 +0900

    feat: 사용자가 좋아요 상품과 게시글 목록 조회를 위한 레포지토리 코드 작성

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -31,4 +31,40 @@ export class UserRepository {
       data: dataToUpdate,
     });
   };
+
+  // 좋아요한 상품 목록 조회
+  findLikedProductsByUserId = async (userId) => {
+    const userWithLikes = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        productLikes: {
+          orderBy: { product: { createdAt: 'desc' } },
+          select: {
+            product: {
+              select: { id: true, name: true, price: true, createdAt: true },
+            },
+          },
+        },
+      },
+    });
+    return userWithLikes.productLikes.map((like) => like.product);
+  };
+
+  // 좋아요한 게시글 목록 조회
+  findLikedArticlesByUserId = async (userId) => {
+    const userWithLikes = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        articleLikes: {
+          orderBy: { article: { createdAt: 'desc' } },
+          select: {
+            article: {
+              select: { id: true, title: true, content: true, createdAt: true },
+            },
+          },
+        },
+      },
+    });
+    return userWithLikes.articleLikes.map((like) => like.article);
+  };
 }
