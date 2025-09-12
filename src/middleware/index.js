b1e8567ad999cf816f882ab93b6a007f8e28b35f
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-09-12 09:17:16 +0900

    fix: 미들웨어 배럴 파일의 오타 수정 및 권한 관련 미들웨어 적용

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,3 +1,5 @@
-export * from './validation.middleware.js';
-export * from './image.middleware.js';
+export * from './validation-middleware.js';
+export * from './image-middleware.js';
 export * from './error-handler.middleware.js';
+export * from './auth-middleware.js';
+export * from './auth-optional-middleware.js';
