d85b0f2b6c5a69681c87b863abb3d428446abcbc
kimhyeyeon <kho1039@naver.com>
2025-09-14 17:08:37 +0900

    edit app.js, readme

--- sprint_mission_4/src/app.js
+++ sprint_mission_4/src/app.js
@@ -13,10 +13,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
-//Passport라는 로그인 시스템이 **"이제부터 로그인 작업을 시작할 거야!"**라고 알려주는 역할
-// 이 코드가 실행되면, Passport는 모든 요청(req)마다 로그인 관련 정보를 담을 수 있는 **빈 가방(초기화된 상태)**을 준비해 줌
-// 덕분에 나중에 로그인할 때 필요한 정보를 이 가방에 깔끔하게 넣을 수 있게 됨
-// 결론적으로, 이 코드는 로그인 시스템이 잘 작동할 수 있도록 제일 먼저 준비를 시키는 아주 중요한 역할을 하는 거예요.
 app.use('/users', userController);
 app.use('/products', productController);
 app.use('/posts', postController);
