d2fb6662bfb444882e06041b1f1cc2ec7768072b
shwon <kairise1234@gmail.com>
2025-09-19 18:17:20 +0900

    mission6 ver2

--- part3-mission4/dist/middlewares/authenticate.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-declare function authenticate(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>> | undefined>;
-export default authenticate;
-//# sourceMappingURL=authenticate.d.ts.map
--- part3-mission4/dist/middlewares/errorHandler.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ErrorRequestHandler } from 'express';
-declare const errorHandler: ErrorRequestHandler;
-export default errorHandler;
-//# sourceMappingURL=errorHandler.d.ts.map
