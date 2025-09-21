04a7a16cffa6caefa284f6195c62d4f33250851e
hj92oo <scfe97@naver.com>
2025-09-21 23:23:12 +0900

    fix the like-service.ts

--- sprint-mission5/src/service/like-service.ts
+++ sprint-mission5/src/service/like-service.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import type { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
-const articleLike = async (req, res) => {
+const articleLike = async (req: Request, res: Response) => {
   const userId = req.user.userId;
-  const articleId = parseInt(req.params.articleId, 10);
+  const articleId = req.params.articleId ? parseInt(req.params.articleId, 10) : undefined;
   const articleLike = await prisma.articleLike.create({
     data: {
       article: { connect: { id: articleId } },
@@ -14,16 +15,16 @@ const articleLike = async (req, res) => {
   res.status(201).send(articleLike);
 }
 
-const articleUnLike = async (req, res) => {
+const articleUnLike = async (req: Request, res: Response) => {
   const articleUnLike = await prisma.articleLike.delete({
     where: { id: Number(req.params.articleLikeId) }
   })
   res.status(201).send(articleUnLike);
 }
 
-const productLike = async (req, res) => {
+const productLike = async (req: Request, res: Response) => {
   const userId = req.user.userId;
-  const productId = parseInt(req.params.productId, 10);
+  const productId = req.params.productId ? parseInt(req.params.productId, 10) : undefined;
   const productLike = await prisma.productLike.create({
     data: {
       product: { connect: { id: productId } },
@@ -33,7 +34,7 @@ const productLike = async (req, res) => {
   res.status(201).send(productLike);
 }
 
-const productUnLike = async (req, res) => {
+const productUnLike = async (req: Request, res: Response) => {
   const productUnLike = await prisma.productLike.delete({
     where: { id: Number(req.params.productLikeId) }
   })
