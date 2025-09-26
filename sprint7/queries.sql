7c6119acdf757edb871e7e609bdb54f115164470
Son JuneYeong <sonj0407@naver.com>
2025-09-26 13:14:46 +0900

    feat: complete queries.sql

--- sprint7/queries.sql
+++ sprint7/queries.sql
@@ -1,16 +1,21 @@
-/*
-  다음 경우들에 대해 총 14개의 SQL 쿼리를 작성해 주세요.
-  예시로 값이 필요한 경우 적당한 값으로 채워넣어서 작성하면 됩니다. 
-*/
+-- psql -h localhost -p 5432 -U postgres   
+-- \c sprint7
+-- /home/sonj0407/sql-practice/sprint7/queries.sql
+
 
 /*
   1. 내 정보 업데이트 하기
   - 닉네임을 "test"로 업데이트
   - 현재 로그인한 유저 id가 1이라고 가정
 */
-FROM user
-  UPDATE nickname = `test`
-WHERE user.id = 1
+
+
+UPDATE "user"
+  SET nickname = 'test'
+WHERE id = 1 ;
+
+SELECT * FROM "user";
+
 
 /*
   2. 내가 생성한 상품 조회
@@ -27,14 +32,16 @@ OFFSET 20
 LIMIT 10
 
 
+
 /*
   3. 내가 생성한 상품의 총 개수
   - 현재 로그인한 유저 id가 1이라고 가정
 */
 SELECT COUNT(*)
-FROM product as p
-GROUP BY p.user_id
-WHERE p.user_id = 1
+  FROM product as p
+WHERE p.user_id = 1;
+
+
 
 
 
@@ -45,14 +52,17 @@ WHERE p.user_id = 1
   - 최신 순으로 정렬
   - 10개씩 페이지네이션, 3번째 페이지
 */
+
 SELECT p.*
-  FROM Product As p
-JOIN `like` AS l
-  ON l.user_id = p.user_id
+  FROM product As p
+JOIN "like" AS l
+  ON l.product_id = p.id
 WHERE p.user_id = 1
 ORDER BY p.created_at DESC
-OFFSET 20
-LIMIT 10 
+-- OFFSET 20
+LIMIT 10 ;
+
+
 
 
 
@@ -61,8 +71,12 @@ LIMIT 10
   5. 내가 좋아요 누른 상품의 총 개수
   - 현재 로그인한 유저 id가 1이라고 가정
 */
-SELECT 
-FROM 
+SELECT *
+FROM product as p
+JOIN "like" as l
+  ON l.product_id= p.id
+WHERE l.user_id = 1;
+
 
 
 
@@ -70,8 +84,9 @@ FROM
   6. 상품 생성
   - 현재 로그인한 유저 id가 1이라고 가정
 */
-INSERT INTO product (image, name, description, price, tag)
-VALUES 
+INSERT INTO product (name, description, image, price, tag, user_id)
+  VALUES ('세번째 물건', '손준영의 물건', 'http:localhost:3000', 40000, ARRAY['첫번째 태그'], 1)
+
 
 /*
   7. 상품 목록 조회
@@ -82,9 +97,11 @@ VALUES
 */
 SELECT *
   FROM product
-WHERE product.name LIKE `%test`
+WHERE product.name LIKE '%test%'
 ORDER BY product.created_at DESC
-LIMIT 10
+LIMIT 10;
+
+
 
 
 /*
@@ -101,23 +118,26 @@ WHERE p.id = 1
   - 1번 상품 수정
 */
 UPDATE product
-  SET colmun =  value
+  SET "description"= '변경된 value 값 입니다'
 
 WHERE id = 1;
+
+
 
 /*
   10. 상품 삭제
   - 1번 상품 삭제
 */
 DELETE FROM product
-  WHERE id = 1 
+  WHERE id = 1;
+
 
 
 /*
   11. 상품 좋아요
   - 1번 유저가 2번 상품 좋아요
 */
-INSERT INTO `like` (user_id, product_id)
+INSERT INTO "like" (user_id, product_id)
   VALUES (1,2) 
 
 
@@ -125,18 +145,21 @@ INSERT INTO `like` (user_id, product_id)
   12. 상품 좋아요 취소
   - 1번 유저가 2번 상품 좋아요 취소
 */
-DELETE WHERE `like`
+DELETE FROM "like"
   WHERE
-   like.user_id = 1 AND
-   like.product_id = 2
+   user_id = 1 AND
+   product_id = 2
+
+
 
 /*
   13. 상품 댓글 작성
   - 1번 유저가 2번 상품에 댓글 작성
 */
-INSERT INTO comment
-VALUES (user_id, product_id,content)
+INSERT INTO comment (user_id, product_id, content)
+VALUES 
   (1,2, '이 상품은 정말 좋아요 ')
+
 
 
 /*
@@ -145,3 +168,12 @@ VALUES (user_id, product_id,content)
   - 최신 순으로 정렬
   - 댓글 날짜 2025-03-25 기준일을 제외한 이전 데이터 10개
 */
+
+SELECT *
+FROM product as p
+JOIN comment as c
+  ON p.id = c.product_id
+WHERE c.updated_at<'2025-03-25 00:00:00'
+ORDER BY c.updated_at DESC
+LIMIT 10
+
--- sprint7/schema.sql
+++ sprint7/schema.sql
@@ -1,20 +1,21 @@
 -- psql -h localhost -p 5432 -U postgres   
--- \c pizza_place
--- /home/sonj0407/sql-practice/sprint-mission-6.sql
+-- \c sprint7
+-- /home/sonj0407/sql-practice/sprint7/schema.sql
 
-
-
-Use 
 
 CREATE TABLE 
-  user (
+  "user" (
     id SERIAL PRIMARY KEY,
     email VARCHAR(25) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     nickname VARCHAR(10),
     created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-  )
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+
+
+
+
 
 
 CREATE TABLE 
@@ -23,14 +24,16 @@ CREATE TABLE
     name VARCHAR(10) NOT NULL,
     description VARCHAR(100) NOT NULL,
     image VARCHAR(100) ,
-    price INTEGER(10) NOT NULL,
+    price INTEGER NOT NULL,
     tag TEXT[],
-    user_id INTEGER(5),
+    user_id INTEGER,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
     CONSTRAINT fk_user
-      FOREIGN KEY (user_id) REFERENCES user(id)
-  )
+      FOREIGN KEY (user_id) REFERENCES "user"(id)
+  );
+
+
 
 CREATE TABLE
   article (
@@ -40,10 +43,12 @@ CREATE TABLE
     image VARCHAR(10) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
-    user_id INTEGER(5),
+    user_id INTEGER,
     CONSTRAINT fk_user
-      FOREIGN KEY (user_id) REFERENCES user(id)
-  )
+      FOREIGN KEY (user_id) REFERENCES "user"(id)
+  );
+
+
 
 CREATE TABLE 
   comment (
@@ -51,27 +56,66 @@ CREATE TABLE
     content VARCHAR(70) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
-    user_id INTEGER(5),
-    article_id INTEGER(5),
+    user_id INTEGER,
+    product_id INTEGER,
     CONSTRAINT fk_user
-      FOREIGN KEY (user_id) REFERENCES user(id),
+      FOREIGN KEY (user_id) REFERENCES "user"(id),
     CONSTRAINT fk_article
-      FOREIGN KEY (article_id) REFERENCES article(id)
-  )
+      FOREIGN KEY (product_id) REFERENCES product(id)
+  );
+
+
+
 
 CREATE TABLE 
-  like (
+  "like" (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER(5),
-    product_id INTEGER(5),
+    user_id INTEGER,
+    product_id INTEGER,
     CONSTRAINT fk_user
-      FOREIGN KEY (user_id) REFERENCES user(id),
+      FOREIGN KEY (user_id) REFERENCES "user"(id),
     CONSTRAINT fk_product
       FOREIGN KEY (product_id) REFERENCES product(id),
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
-  )
+  );
   
+
+
+
+
+
+--  Seeding 하기 
+
+-- 유저 생성
+INSERT INTO "user" (email,password,nickname)
+  VALUES 
+    ('sonj0407@naver.com', '1234', '손준영'),
+    ('sonj0407@daum.net', '1234', '손준영2');
+
+-- product 생성
+INSERT INTO product (name, description, image, price, tag, user_id)
+  VALUES ('첫번째 물건', '손준영의 물건', 'http:localhost:3000', 40000, ARRAY['첫번째 태그'], 1)
+
+-- article 생성 
+INSERT INTO article (title,content,image,user_id)
+  VALUES 
+    ('첫번째 기사', '기사의 내용' , 'naver.com' , 1),
+    ('두번째 기사', '기사의 내용' , 'naver.com' , 1);
+
+
+-- comment 생성 
+INSERT INTO comment (content, user_id, product_id)
+  VALUES 
+    ('와 이 상품 정말 좋군요', 1, 1),
+    ('생각보다 괜찮더라구요', 2, 1);
+
+
+-- like 생성 
+INSERT INTO "like" (user_id, product_id)
+  VALUES 
+    (1 , 1);
+
 
 
 
@@ -79,8 +123,8 @@ CREATE TABLE
   -- like n:m 모델 생성하기                    완료
   -- tag 어떻게 구상할지 생각하기               완료
   -- default now 문법 맞는지 검사하기          완료
-  -- seeding 하기 
+  -- seeding 하기                            완료
   -- image에 string만 담을지, 이미지 자체를 담을지 생각하기    완료 
-  -- psql 접속해서 데이터베이스 생성하고, 데이터 베이스에 들어가고, 테이블 생성하기 
+  -- psql 접속해서 데이터베이스 생성하고, 데이터 베이스에 들어가고, 테이블 생성하기 완료
   -- 문제들 실제로 작동하는지 체크하기           
   -- article, product에 각각 comment를 적용할 것인지?
