7719de7cd8020c40b822ab74bd8f99a24f81a531
예찬 주 <jooyc135@DESKTOP-6DCVU9O.localdomain>
2026-01-20 11:47:26 +0900

    Mission12

---
+++ Mission12/Selection_sort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+}
+const nums = [3, 1, 2];
+console.log(nums); // [3, 1, 2];
+selectionSort(nums);
+console.log(nums); // [1, 2, 3]
