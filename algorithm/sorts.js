b11f6d0bef9ef1d13f22662dad9dcdfafc99b05d
mone <qhdltmwhs@gmail.com>
2026-01-22 11:18:07 +0900

    feat: 힙 정렬 추가

--- algorithm/sorts.js
+++ algorithm/sorts.js
@@ -89,9 +89,43 @@ function partition(arr, left, right) {
   return i + 1;
 }
 
+/** 힙 정렬 - 원본 배열 수정 */
+function heapSort(arr) {
+  const n = arr.length;
+
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+}
+
+function heapify(arr, n, i) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, n, largest);
+  }
+}
+
 module.exports = {
   selectionSort,
   insertionSort,
   mergeSort,
   quickSort,
+  heapSort,
 };
