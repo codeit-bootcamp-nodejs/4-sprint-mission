40859f31c6e1280f005cb02aa05b23d6bcdbfefe
이봉준 <bjl0615@gmail.com>
2026-01-26 14:11:15 +0900

    [이봉준] 스프린트 미션 13

---
+++ SPRINT_MISSION_13/algorithm/Queue.js
@@ -0,0 +1,29 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(value) {
+    this.items.push(value);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.shift();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+module.exports = Queue;
